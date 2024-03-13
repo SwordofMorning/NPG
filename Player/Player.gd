575206d06ecd8c0cfdcd6bf406f799a0e2e25dd0
Morning <969211735@qq.com>
2024-03-13 20:54:13 +0800

    Feat: input key.
    Add new fet in Player.gd, use signal to coustom input key event.

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,34 +8,54 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anime = get_node("AnimationPlayer")
 
+signal move_jump
+signal move_left
+signal move_right
+signal move_stop
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_W:
+			emit_signal("move_jump")
+		elif event.keycode == KEY_A:
+			emit_signal("move_left")
+		elif event.keycode == KEY_D:
+			emit_signal("move_right")
+	elif event is InputEventKey and event.is_released():
+		if event.keycode == KEY_W:
+			pass
+		elif event.keycode == KEY_A or event.keycode == KEY_D:
+			emit_signal("move_stop")
+
+func _ready():
+	connect("move_jump", _on_move_jump, 0)
+	connect("move_left", _on_move_left, 0)
+	connect("move_right", _on_move_right, 0)
+	connect("move_stop", _on_move_stop, 0)
+	
+func _on_move_jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		anime.play("Jump")
+		
+func _on_move_left():
+	velocity.x = -1 * SPEED
+	anime.play("Run")
+	get_node("AnimatedSprite2D").flip_h = true
+		
+func _on_move_right():
+	velocity.x = 1 * SPEED
+	anime.play("Run")
+	get_node("AnimatedSprite2D").flip_h = false
+
+func _on_move_stop():
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	anime.play("Idle")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anime.play("Jump")
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	# Change Anime Direction
-	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-	elif direction == 1:
-		get_node("AnimatedSprite2D").flip_h = false
-	
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			anime.play("Run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			anime.play("Idle")
 		
 	move_and_slide()
 	
