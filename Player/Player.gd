5ce278001db84b5d7037411d0d7911995f875d35
SwordofMorning <969211735@qq.com>
2024-03-14 11:24:45 +0800

    Fix: Use InputMap to Replace Signal
    1. in Player.gd, use InputMap to handle input now. Previous use Input.event_code and Signals to handle.

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,9 +2,13 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+# How many times ghosts are generated during dash
+const DASH_TIMES = 10
+# iterator for DASH_TIMES
+var dash_remain_times = DASH_TIMES
 const DASH_SPEED_MULTIPLE = 5
-const DASH_REMAIN = 10
-var dash_remain = DASH_REMAIN
+const DASH_SPEED_DEFAULT = 1
+var speed_multiple = DASH_SPEED_DEFAULT
 
 # -1 is left, 1 is right
 var player_direction = 0
@@ -16,84 +20,55 @@ var Ghost = preload("res://Player/Ghost.tscn")
 
 @onready var anime = get_node("AnimationPlayer")
 
-signal move_jump
-signal move_left
-signal move_right
-signal move_stop
-signal move_dash
-
-func _input(event):
-	if event is InputEventKey and event.is_pressed():
-		if event.keycode == KEY_W:
-			emit_signal("move_jump")
-		elif event.keycode == KEY_A:
-			emit_signal("move_left")
-		elif event.keycode == KEY_D:
-			emit_signal("move_right")
-		elif event.keycode == KEY_SHIFT:
-			emit_signal("move_dash")
-	elif event is InputEventKey and event.is_released():
-		if event.keycode == KEY_W or event.keycode == KEY_SHIFT:
-			pass
-		elif event.keycode == KEY_A or event.keycode == KEY_D:
-			emit_signal("move_stop")
-
 func _ready():
-	connect("move_jump", _on_move_jump, 0)
-	connect("move_left", _on_move_left, 0)
-	connect("move_right", _on_move_right, 0)
-	connect("move_stop", _on_move_stop, 0)
-	connect("move_dash", _on_move_dash, 0)
-	
-func _on_move_jump():
-	if is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anime.play("Jump")
-		
-func _on_move_left():
-	player_direction = -1
-	velocity.x = player_direction * SPEED
-	anime.play("Run")
-	get_node("AnimatedSprite2D").flip_h = true
-		
-func _on_move_right():
-	player_direction = 1
-	velocity.x = player_direction * SPEED
-	anime.play("Run")
-	get_node("AnimatedSprite2D").flip_h = false
-
-func _on_move_stop():
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-	anime.play("Idle")
-	
-func _on_move_dash():
-	get_node("DashTimer").start()
-	velocity.x += player_direction * SPEED * DASH_SPEED_MULTIPLE
-	gravity = 0
+	pass
 	
 func _on_dash_timer_timeout():
-	if dash_remain > 0:
-		dash_remain -= 1
+	if dash_remain_times > 0:
+		dash_remain_times -= 1
 		var ghost = Ghost.instantiate()
 		ghost.position = position
 		ghost.get_node("Sprite2D").flip_h = get_node("AnimatedSprite2D").flip_h
 		get_parent().add_child(ghost)
 	else:
-		velocity.x -= player_direction * SPEED * DASH_SPEED_MULTIPLE
-		dash_remain = DASH_REMAIN
+		speed_multiple = DASH_SPEED_DEFAULT
+		dash_remain_times = DASH_TIMES
 		get_node("DashTimer").stop()
 		gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		
-	move_and_slide()
-	
+func Player_Death_or_Out_Game():
 	if (Global.Health <= 0) or (Global.enemy_nums < 0):
 		#queue_free()
 		Utils.SaveGame()
 		var world = get_parent().get_parent()
 		world.Out_World()
-		
+
+func _physics_process(delta):
+# Grvaity
+	if not is_on_floor(): velocity.y += gravity * delta
+# Jump
+	if Input.is_action_pressed("Move_Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		anime.play("Jump")
+# Left and Right Move		
+	if Input.is_action_pressed("Move_Left"):
+		player_direction = -1
+		anime.play("Run")
+		get_node("AnimatedSprite2D").flip_h = true
+	elif Input.is_action_pressed("Move_Right"):
+		player_direction = 1
+		anime.play("Run")
+		get_node("AnimatedSprite2D").flip_h = false
+	elif Input.is_action_just_released("Move_Left") or Input.is_action_just_released("Move_Right"):
+		player_direction = 0
+		anime.play("Idle")
+# Dash
+	if Input.is_action_pressed("Move_Dash"):
+		get_node("DashTimer").start()
+		gravity = 0
+		speed_multiple = DASH_SPEED_MULTIPLE
+
+	velocity.x = player_direction * SPEED * speed_multiple
+	
+	move_and_slide()
+	Player_Death_or_Out_Game()
